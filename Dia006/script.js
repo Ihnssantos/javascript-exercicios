0ba952e5f91d6ffad35fea5ebab41726a78d3ab9
Igor Santos <igorhnss@gmail.com>
2026-09-20 11:26:39 -0300

    Dia006 - Estrutura condicionais em JavaScript

---
+++ Dia006/script.js
@@ -0,0 +1,11 @@
+let idade = 11;
+
+if (idade >= 60) {
+    console.log("Idoso");
+} else if (idade >= 18) {
+    console.log("Adulto")
+} else if (idade >= 12) {
+    console.log("Adolescente");
+} else {
+    console.log("Criança");
+}
