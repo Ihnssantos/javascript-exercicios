ebdada0ec1f424a9715c7c63434a0f99c01cf453
Igor Santos <igorhnss@gmail.com>
2026-09-20 18:39:46 -0300

    Dia014 - Funcao normal e arrow function

---
+++ Dia014/script.js
@@ -0,0 +1,15 @@
+//funcao normal
+function mensagem1() {
+    console.log("Estou praticando JavaScript!");
+};
+
+mensagem1();
+
+
+
+//arrow function
+let mensagem2 = () => {
+    console.log("Estou praticando JavaScript!!");
+}
+
+mensagem2();
