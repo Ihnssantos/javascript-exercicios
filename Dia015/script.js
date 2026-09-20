3532e3e325506b23dc79d0bff0ca4b89e68e9726
Igor Santos <igorhnss@gmail.com>
2026-09-20 18:47:02 -0300

    Dia015 - Parametros em funcoes

---
+++ Dia015/script.js
@@ -0,0 +1,17 @@
+function apresentarPessoa1(nome, idade, cidade) {
+    console.log("Nome: " + nome);
+    console.log("Idade: " + idade);
+    console.log("cidade: " + cidade);
+}
+
+apresentarPessoa1("Igor", 25, "Congonhas");
+
+
+
+const apresentarPessoa2 = (nome, idade, cidade) => {
+    console.log("Nome: " + nome);
+    console.log("Idade: " + idade);
+    console.log("cidade: " + cidade);
+}
+
+apresentarPessoa2("João", 30, "São paulo");
