8a59d0ea1c0ac370062128c2cc52ba679d9a43bf
Igor Santos <igorhnss@gmail.com>
2026-09-20 11:03:54 -0300

    Dia004 - operadores de comparacao em JavaScript

---
+++ Dia004/script.js
@@ -0,0 +1,16 @@
+let numero1 = 5;
+let numero2 = 3;
+
+let maior = numero1 > numero2;
+let menor = numero1 < numero2;
+let maiorOuIgual = numero1 >= numero2;
+let menorOuIgual = numero1 <= numero2;
+let igual = numero1 === numero2; //tipo e o valor
+let diferente = numero1 !== numero2; //tipo e o valor
+
+console.log(maior);
+console.log(menor);
+console.log(maiorOuIgual);
+console.log(menorOuIgual);
+console.log(igual);
+console.log(diferente);
