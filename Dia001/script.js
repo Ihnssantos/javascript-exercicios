0e95200f2e6188bd53ae61410586e5c5d26f1e05
Igor Santos <igorhnss@gmail.com>
2026-09-20 10:44:09 -0300

    Dia001 - Declaracao de variaveis em JavaScript

---
+++ Dia001/script.js
@@ -0,0 +1,11 @@
+const nome = "Igor";
+let idade = 25;
+let cidade = "Congonhas";
+let altura = 1.78;
+let estudandoJavaScript = true;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(altura);
+console.log(estudandoJavaScript);
