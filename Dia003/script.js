7c6abd5f1adfcc92f27c4c053f41479ac9ec673e
Igor Santos <igorhnss@gmail.com>
2026-09-20 10:57:31 -0300

    Dia003 - operadores aritmeticos em JavaScript

---
+++ Dia003/script.js
@@ -0,0 +1,14 @@
+let numero1 = 2;
+let numero2 = 2;
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+let resto = numero1 % numero2;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(resto);
