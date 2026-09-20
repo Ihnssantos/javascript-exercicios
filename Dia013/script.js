a8fe3c07697cdc1305521af198b1e11f368f87ad
Igor Santos <igorhnss@gmail.com>
2026-09-20 18:15:51 -0300

    entrada e saida de dados

---
+++ Dia013/script.js
@@ -0,0 +1,10 @@
+//Entrada de dados no navegador
+let nome = prompt("Digite seu nome: ");
+let idade = prompt("Digite sua idade: ");
+let cidade = prompt("Digite qual cidade você mora: ");
+
+
+//Saida de dados no console no navegador
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Cidade: " + cidade);
