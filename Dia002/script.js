b111dda10161caf65b853d330392b78ee4b3fea6
Igor Santos <igorhnss@gmail.com>
2026-09-20 10:52:34 -0300

    Dia002 - tipos de dados em JavaScript

---
+++ Dia002/script.js
@@ -0,0 +1,13 @@
+const nome = "Igor"; //String
+let idade = 25; //number
+let altura = 1.78; //number
+let estudandoJavaScript = true; //boolean
+let valorIndefinido = undefined; //undefined
+let valorNulo = null; //nulo
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof altura);
+console.log(typeof estudandoJavaScript);
+console.log(typeof valorIndefinido);
+console.log(typeof valorNulo);
