6b204b3d46c9df478fb397ce6c68095e6fb8eea5
Igor Santos <igorhnss@gmail.com>
2026-09-22 18:42:21 -0300

    Dia023 - push e pop em array

---
+++ Dia023/script.js
@@ -0,0 +1,12 @@
+const linguagens = ["JavaScript", "Java", "Python"];
+
+linguagens.push("C#"); //adciona item no final do array
+
+console.log(linguagens);
+
+let removido = linguagens.pop(); //remove o ultimo item
+
+console.log(removido); //mostra o ultimo item removido
+console.log(linguagens); //mostra o array final
+
+
