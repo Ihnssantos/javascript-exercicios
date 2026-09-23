4d2820bdab879a27118cb51ece9c6845b6cf31bb
Igor Santos <igorhnss@gmail.com>
2026-09-22 23:54:09 -0300

    Dia027 - for of em arrays

---
+++ Dia027/script.js
@@ -0,0 +1,12 @@
+const linguagens = ["JavaScript", "Java", "Python", "C#", "PHP"];
+
+for (const linguagem of linguagens) {
+    console.log("Linguagem: " + linguagem);
+}
+
+
+const numeros = [10, 20, 30, 40, 50];
+
+for (const n of numeros) {
+    console.log(n * 2);
+}
