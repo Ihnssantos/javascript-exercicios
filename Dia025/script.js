bc2d2549f957f513709bf6d82e6bccdf00c8d6b3
Igor Santos <igorhnss@gmail.com>
2026-09-22 23:43:16 -0300

    includes e indexOf em arrays

---
+++ Dia025/script.js
@@ -0,0 +1,7 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "React", "Node"];
+
+console.log(tecnologias.includes("JavaScript"));
+console.log(tecnologias.includes("Angular"));
+
+console.log(tecnologias.indexOf("React"));
+console.log(tecnologias.indexOf("Java"));
