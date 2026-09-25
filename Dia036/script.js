7d9a8daf47b89d1d41ff0aa3859e54e513b5cc0b
Igor Santos <igorhnss@gmail.com>
2026-09-25 20:57:55 -0300

    Dia036 - criacao e acesso de objetos

---
+++ Dia036/script.js
@@ -0,0 +1,11 @@
+const carro = {
+    marca: "Ford",
+    modelo: "Focus",
+    ano: 2009,
+    cor: "Preto"
+}
+
+console.log(carro.marca);
+console.log(carro.modelo);
+console.log(carro.ano);
+console.log(carro.cor);
