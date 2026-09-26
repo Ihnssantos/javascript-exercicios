fdd6a34973634d828c0fb1bf131815030880bafc
Igor Santos <igorhnss@gmail.com>
2026-09-26 17:16:09 -0300

    Dia051 - Template literals

---
+++ Dia051/script.js
@@ -0,0 +1,5 @@
+const produto = "Notebook";
+const preco = 3500;
+const quantidade = 2;
+
+console.log(`Produto: ${produto} | Preço: R$ ${preco} | Quantidade: ${quantidade} | Total: R$ ${preco * quantidade}`);
