c6517b07c9fecd56b7bafce4912b7cf736f7c5b4
Igor Santos <igorhnss@gmail.com>
2026-09-25 21:07:33 -0300

    Dia038 - acesso de propriedades em objetos

---
+++ Dia038/script.js
@@ -0,0 +1,12 @@
+const usuario = {
+    nome: "Igor",
+    idade: 25,
+    email: "igor@gmail.com"
+}
+
+const campo = "email";
+
+
+console.log(usuario.nome);
+console.log(usuario["idade"]);
+console.log(usuario[campo]);
