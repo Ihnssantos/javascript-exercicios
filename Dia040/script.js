27b322c39651bbfc2c51bdd0ff83e673b1bcd6c4
Igor Santos <igorhnss@gmail.com>
2026-09-25 21:24:14 -0300

    objetos aninhados

---
+++ Dia040/script.js
@@ -0,0 +1,14 @@
+const produto = {
+    nome: "Notebook",
+    preco: 3500,
+
+    fabricante: {
+        nome: "Lenovo",
+        pais: "China"
+    }
+};
+
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.fabricante.nome);
+console.log(produto.fabricante.pais);
