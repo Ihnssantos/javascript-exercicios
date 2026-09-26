19a3959fe84c895e25e080363ed3d0a7025ff653
Igor Santos <igorhnss@gmail.com>
2026-09-26 17:28:52 -0300

    Dia053 - Destructuring de arrays

---
+++ Dia053/script.js
@@ -0,0 +1,10 @@
+const linguagens = [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "Java"
+];
+
+const [primeira, segunda, terceira, quarta] = linguagens;
+
+console.log(`Principal: ${primeira} | Próxima: ${segunda} | Outras: ${terceira} e ${quarta}`);
