e67311a8add36959eeee72734a103ab56d719ef2
Igor Santos <igorhnss@gmail.com>
2026-09-25 21:29:38 -0300

    Dia041 - arrays de objetos

---
+++ Dia041/script.js
@@ -0,0 +1,9 @@
+const produtos = [
+    {nome: "Notebook", preco: 3500},
+    {nome: "Mouse", preco: 150},
+    {nome: "Teclado", preco: 300}
+];
+
+console.log(produtos[0].nome);
+console.log(produtos[1].preco);
+console.log(produtos[2].nome);
