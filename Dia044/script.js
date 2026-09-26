ba0d01361b1e0d0456acecc74541eea228d45028
Igor Santos <igorhnss@gmail.com>
2026-09-25 22:20:23 -0300

    Dia044 - metodos Object

---
+++ Dia044/script.js
@@ -0,0 +1,15 @@
+const jogo = {
+    nome: "Red Dead Redemption 2",
+    ano: 2018,
+    genero: "Ação e aventura",
+    nota: 10
+};
+
+
+const propriedades = Object.keys(jogo);
+const valores = Object.values(jogo);
+const entradas = Object.entries(jogo);
+
+console.log(propriedades);
+console.log(valores);
+console.log(entradas);
